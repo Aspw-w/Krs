8f3890f16ff116147c2f95abb79c593ba2d3e35f
Aspw <75427057+Aspw-w@users.noreply.github.com>
2026-08-13 23:16:25 +0900

    Fix duplicated LivingEntityRenderMixin.java

--- src/client/java/com/instrumentalist/krs/hacks/features/render/TrueSight.java
+++ src/client/java/com/instrumentalist/krs/hacks/features/render/TrueSight.java
@@ -2,24 +2,19 @@ package com.instrumentalist.krs.hacks.features.render;
 
 import com.instrumentalist.krs.hacks.Module;
 import com.instrumentalist.krs.hacks.ModuleCategory;
-import com.instrumentalist.krs.hacks.ModuleManager;
 import com.instrumentalist.krs.utils.value.BooleanValue;
-import com.instrumentalist.krs.utils.value.FloatValue;
 import org.lwjgl.glfw.GLFW;
 
 public class TrueSight extends Module {
 
     @Setting
-    public static final BooleanValue entities = new BooleanValue("Entities", true);
+    public static final BooleanValue entities = new BooleanValue("Entities", false);
 
     @Setting
-    public static final BooleanValue barriers = new BooleanValue("Barriers", false);
-
-    @Setting
-    public static final FloatValue entityAlpha = new FloatValue("EntityAlpha", 50, 0, 100);
+    public static final BooleanValue barriers = new BooleanValue("Barriers", true);
 
     public TrueSight() {
-        super("TrueSight", ModuleCategory.Render, GLFW.GLFW_KEY_UNKNOWN, false, false);
+        super("True Sight", ModuleCategory.Render, GLFW.GLFW_KEY_UNKNOWN, false, true);
     }
 
     @Override
@@ -28,18 +23,6 @@ public class TrueSight extends Module {
 
     @Override
     public void onDisable() {
-    }
-
-    public static boolean shouldRenderEntity(net.minecraft.world.entity.LivingEntity entity) {
-        if (!ModuleManager.getModuleState(TrueSight.class)) {
-            return false;
-        }
-
-        if (!entities.get()) {
-            return false;
-        }
-
-        return entity.isInvisible();
     }
 
     public static boolean requiresTrueSight(net.minecraft.world.entity.Entity entity) {
