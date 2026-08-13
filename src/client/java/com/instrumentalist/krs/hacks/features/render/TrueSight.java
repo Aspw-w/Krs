7aa6426c81ce6c1b714bfc2d931424e3bfabcc07
pro5383 <95685699+pro5383@users.noreply.github.com>
2026-08-13 06:41:47 +0200

    Remove unused barrier constants from TrueSight
    Removed unused constants related to barrier gizmo color and scan radius.

--- src/client/java/com/instrumentalist/krs/hacks/features/render/TrueSight.java
+++ src/client/java/com/instrumentalist/krs/hacks/features/render/TrueSight.java
@@ -9,10 +9,6 @@ import org.lwjgl.glfw.GLFW;
 
 public class TrueSight extends Module {
 
-    public static final int BARRIER_GIZMO_COLOR = 0xFFFF0000;
-    public static final int BARRIER_SCAN_RADIUS_XZ = 32;
-    public static final int BARRIER_SCAN_RADIUS_Y = 32;
-
     @Setting
     public static final BooleanValue entities = new BooleanValue("Entities", true);
 
